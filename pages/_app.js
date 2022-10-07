32b82d48efc69ed3b048038e3657779cf9f2d4a6
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-10-07 19:29:59 +0530

    add fallback meta

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
