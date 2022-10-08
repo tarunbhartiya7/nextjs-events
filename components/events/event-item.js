4cc20f6233724a60c3566c7b14263fcaecfe8b0a
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-10-08 10:44:46 +0530

    add image optimisation

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import Button from '../ui/button'
 import DateIcon from '../icons/date-icon'
 import AddressIcon from '../icons/address-icon'
 import ArrowRightIcon from '../icons/arrow-right-icon'
+import Image from 'next/image'
 
 export default function EventItem({ title, image, date, location, id }) {
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -15,7 +16,7 @@ export default function EventItem({ title, image, date, location, id }) {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
