7600d0ba76c480cceaed872e5871c817c8631d48
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-10-11 16:19:36 +0530

    show notification

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,12 +1,13 @@
-import { useEffect, useInsertionEffect, useLayoutEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import CommentList from './comment-list'
 import NewComment from './new-comment'
 import classes from './comments.module.css'
+import NotificationContext from '../../store/notification-context'
 
 function Comments(props) {
   const { eventId } = props
-
+  const notificationCtx = useContext(NotificationContext)
   const [showComments, setShowComments] = useState(false)
   const [comments, setComments] = useState([])
 
@@ -23,16 +24,36 @@ function Comments(props) {
   }
 
   async function addCommentHandler(commentData) {
-    const response = await fetch(`/api/comments/${eventId}`, {
-      method: 'POST',
-      body: JSON.stringify(commentData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    notificationCtx.showNotification({
+      title: 'Sending comment...',
+      message: 'Your comment is currently being stored.',
+      status: 'pending',
     })
 
-    const result = await response.json()
-    console.log(result)
+    try {
+      const response = await fetch(`/api/comments/${eventId}`, {
+        method: 'POST',
+        body: JSON.stringify(commentData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      const result = await response.json()
+      console.log(result)
+
+      notificationCtx.showNotification({
+        title: 'Success!',
+        message: 'Your comment was saved.',
+        status: 'success',
+      })
+    } catch (error) {
+      notificationCtx.showNotification({
+        title: 'Error!',
+        message: error.message || 'Something went wrong!',
+        status: 'error',
+      })
+    }
   }
 
   return (
